38f19d9d82a525ebfad1ee6b8a100f7eb95d4a5b
RusinovArtem <rusinov_artem@yahoo.com>
2020-04-10 11:56:47 +0300

    Try to compile gtest

--- make.php
+++ make.php
@@ -10,12 +10,14 @@ echo "This is better make file\n";
 
 //clean everything
      `rm prog`;
+     `rm test`;
      `rm build -rf`;
      `mkdir build`;
 
 // build some object files     
      `g++ -c -o build/main.o main.cpp`;
      `g++ -c -o build/func.o func.cpp`;
+     `g++ -c -o build/test1.o test1.cpp`;
 
 //build static library 
      `g++ -c -o build/staticlib.o staticlib.cpp`;
@@ -28,7 +30,9 @@ echo "This is better make file\n";
      //link it all together
      $dir = __DIR__;
      `g++ -o build/prog build/main.o build/func.o build/staticlib.a {$dir}/build/sharedlib.so`;
+     `g++ -o build/test1 build/test1.o -pthread /root/googletest-release-1.10.0/build/lib/libgtest.a /root/googletest-release-1.10.0/build/lib/libgtest_main.a `;
      `ln -s build/prog prog`;
+     `ln -s build/test1 test`;
 
 echo "finish\n";
 
